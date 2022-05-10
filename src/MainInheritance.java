249c5093fdbc9dd7bc7cc1aaa56da5d36334afc6
Your Name <k.lipski777@gmail.com>
2022-05-11 00:50:16 +0200

    Tekst wiadomosci commita 6

--- src/MainInheritance.java
+++ src/MainInheritance.java
@@ -2,9 +2,11 @@ public class MainInheritance {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         // dziedziczenie - ang. ingeritance
+
         BasicClass basicClass = new BasicClass();
         ExpandedClass expandedClass = new ExpandedClass();
 
+        expandedClass.expandedMethod();
         
     }
 }
